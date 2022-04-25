8487d2aeaa4ecc13af2834ece29fdccf0e7bc8e1
NicolasJavierJan <nicolasjavierjan@gmail.com>
2022-04-25 14:23:16 +0200

    hi

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,20 +6,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
-@Controller
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	public String index(Model model){
-		//In order to make it work we need to create a "home" directory inside "Templates" inside "resources".
-		// Once the folder is called "templates.home" create an "index.html" file inside
-		return "home/index";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
 
