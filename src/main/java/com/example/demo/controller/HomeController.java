9e7966af1fa99e00864f36a633368a7b33712020
NicolasJavierJan <nicolasjavierjan@gmail.com>
2022-04-25 14:30:28 +0200

    praise the lord

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,15 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 //Annotation to let Spring know that this is a controller class
 @Controller
 public class HomeController {
-
-
-
     //get the mapping for index (home) page
     @GetMapping("/")
     public String index(Model model){
         //In order to make it work we need to create a "home" directory inside "Templates" inside "resources".
         // Once the folder is called "templates.home" create an "index.html" file inside
-        return "templates/home/index.html";
+        return "Hi";
     }
 
 }
